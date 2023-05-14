6fb6c3043470aaffb89c68914ca2e241340a09b5
alex28042 <alex28042@gmail.com>
2023-05-14 22:44:06 +0200

    .env

--- src/users/infrastructure/authController.ts
+++ src/users/infrastructure/authController.ts
@@ -1,14 +1,23 @@
 import {Request, Response} from "express";
 import {AuthService} from "../application/authService";
 import {User} from "../domain/user";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv"
+dotenv.config();
 
 export class AuthController {
 
     constructor(readonly authService: AuthService) {}
+
     async login(req: Request, res: Response) {
         const userEmail = req.body.email;
 
         const user = await this.authService.login(userEmail);
+
+        if (!process.env.ACCESS_TOKEN) {
+            res.status(404).send({status: "FAILED", data: { message: "Error register" }})
+            return;
+        }
 
         res.status(200).send({status: "OK", data: { ...user }})
     }
@@ -18,6 +27,11 @@ export class AuthController {
 
         const user = await this.authService.register(userRegister);
 
-        res.status(200).send({status: "OK", data: { insertedId: user }})
+        if (!process.env.ACCESS_TOKEN) {
+            res.status(404).send({status: "FAILED", data: { message: "Error register" }});
+            return;
+        }
+
+        res.status(200).send({status: "OK", data: { insertedId: jwt.sign(user, process.env.ACCESS_TOKEN) }});
     }
 }
