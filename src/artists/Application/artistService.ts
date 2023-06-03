e565b522f0b948c8c908e7e91a01fa62542dddf1
alex28042 <alex28042@gmail.com>
2023-06-03 13:20:13 +0200

    artist service

--- src/artists/Application/artistService.ts
+++ src/artists/Application/artistService.ts
@@ -1,4 +1,17 @@
+import {MongoArtistRepository} from "../Infrastructure/mongoArtistRepository";
+import {Artist} from "../Domain/artist";
+
 export class ArtistService {
-    constructor() {
+    constructor(private readonly ArtistRepository: MongoArtistRepository) {}
+
+    async getArtistById(artistId: string) {
+        return;
+    }
+
+    async getArtistByName(artistName: string) {
+        return;
+    }
+    async createArtist(artist: Artist) {
+        return;
     }
 }
