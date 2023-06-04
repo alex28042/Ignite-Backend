a72f6458e03249cfb5c14c063f7bfa95135ec7fd
alex28042 <alex28042@gmail.com>
2023-06-04 21:01:17 +0200

    artist controller

--- src/artists/Infrastructure/artistController.ts
+++ src/artists/Infrastructure/artistController.ts
@@ -19,15 +19,24 @@ export class ArtistController {
     async getArtistByName(req: Request, res: Response) {
         const artistName = req.params.name as string;
 
-        const artist = await this.artistService.getArtistByName(artistName)
+        try {
+            const artist = await this.artistService.getArtistByName(artistName)
 
-        res.status(200).send({status: "OK", data: {...artist}})
+            res.status(200).send({status: "OK", data: {...artist}})
+        } catch (e) {
+            res.status(500).send({status: "FAILED", error: `artist by ${artistName} not found`})
+
+        }
     }
     async createArtist(req: Request, res: Response) {
         const artist = new Artist(req.body.name, req.body.musicsGender, req.body.description);
 
-        const artistId = await this.artistService.createArtist(artist)
+        try {
+            const artistId = await this.artistService.createArtist(artist)
 
-        res.status(200).send({status: "OK", data: {insertedId: artistId, ...artist}})
+            res.status(200).send({status: "OK", data: {insertedId: artistId, ...artist}})
+        } catch (e) {
+            res.status(500).send({status: "FAILED", error: `error creating artist`})
+        }
     }
 }
